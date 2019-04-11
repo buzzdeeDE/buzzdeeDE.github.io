a7cdd7c3ed47dc29aff4cb0771c1b3038823195e
Sebastian Rackwitz <rackwitz@insaneagency.de>
2019-04-11 19:41:08 +0200

    Updates

---
+++ precache-manifest.fe488e9d8d9820fb490f35e441931b95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/buzzdeeDE.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d118d7f287d94e1cec2d",
+    "url": "/buzzdeeDE.github.io/static/js/runtime~main.3e98bc09.js"
+  },
+  {
+    "revision": "c73a7be1c42593eaa705",
+    "url": "/buzzdeeDE.github.io/static/js/main.c1c86f2c.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/buzzdeeDE.github.io/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c73a7be1c42593eaa705",
+    "url": "/buzzdeeDE.github.io/static/css/main.a977b7c2.chunk.css"
+  },
+  {
+    "revision": "818c78ee9679a0b3b382593a3aae688a",
+    "url": "/buzzdeeDE.github.io/index.html"
+  }
+];
