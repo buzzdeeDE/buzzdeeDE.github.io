98cdebeab3829e3714df17cb030ea0b9808e326c
Sebastian <rackwitz@insaneagency.de>
2019-08-14 20:00:20 +0200

    Updates

---
+++ precache-manifest.b0e02ef9abf5d5a1c392f277a44ee2d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "33587e6bbf35d1f5b789",
+    "url": "/static/js/main.486f1353.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "33587e6bbf35d1f5b789",
+    "url": "/static/css/main.5b488fc7.chunk.css"
+  },
+  {
+    "revision": "9b90d5a5556c320ecbf74e160d4848c7",
+    "url": "/index.html"
+  }
+];
